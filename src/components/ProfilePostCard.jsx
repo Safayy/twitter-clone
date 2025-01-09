b267edd31336a9a8e4176ae0eeb3ea1165fbff82
Safa Yousif <safa.yousif@outlook.com>
2025-01-09 08:45:46 +0800

    Intial Commit

---
+++ src/components/ProfilePostCard.jsx
@@ -0,0 +1,96 @@
+import axios from "axios";
+// import { jwtDecode } from "jwt-decode";
+import { useContext, useEffect, useState } from "react";
+import { Button, Col, Image, Row } from "react-bootstrap";
+
+import { useDispatch } from "react-redux";
+import { likePost, removeLikeFromPost } from "../features/posts/postsSlice";
+import { AuthContext } from './AuthProvider'
+
+export default function ProfilePostCard({ post }) {
+  const { content, id: postId, imageUrl} = post;
+  const [likes, setLikes] = useState(post.likes || []);
+  const dispatch = useDispatch();
+  const { currentUser } = useContext(AuthContext);
+  const userId = currentUser.uid;
+  const isLiked = likes.includes(userId);
+  const handleLike = () => (isLiked ? removeFromLikes() : addToLikes());
+
+  // Decoding to get the userId
+  const token = localStorage.getItem("authToken");
+  const decode = 'jwtDecode(token)'
+  // const userId = decode.id;
+
+  const pic = "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
+ 
+  const addToLikes = () => {
+    setLikes([...likes, userId]);
+    // axios.post(`${BASE_URL}/likes`, {
+    //   user_id: userId,
+    //   post_id: postId,
+    // })
+    //   .then((response) => {
+    //     setLikes([...likes, { ...response.data, likes_id: response.data.id }]);
+    //   })
+    //   .catch((error) => console.error("Error:", error))
+  }
+
+  const removeFromLikes = () => {
+    setLikes(likes.filter((id) => id !== userId))
+    dispatch(removeLikeFromPost({userId}, postId))
+    // const like = likes.find((like) => like.user_id === userId);
+    // if (like) {
+    //   axios
+    //     .put(`${BASE_URL}/likes/${userId}/${postId}`) // Include userId and postId in the URL
+    //     .then(() => {
+    //       // Update the state to reflect the removal of the like
+    //       setLikes(likes.filter((likeItem) => likeItem.user_id !== userId));
+    //     })
+    //     .catch((error) => console.error("Error:", error));
+    // }
+  };
+
+  return (
+    <Row
+      className="p-3"
+      style={{
+        borderTop: "1px solid #D3D3D3",
+        borderBottom: "1px solid #D3D3D3"
+      }}
+    >
+      <Col sm={1}>
+        <Image src={pic} fluid roundedCircle />
+      </Col>
+
+      <Col>
+        <strong>Haris</strong>
+        <span> @haris.samingan · Apr 16</span>
+        <p>{content}</p>
+        <Image src={imageUrl} style={{width:150}}/>
+        <div className="d-flex justify-content-between">
+          <Button variant="light">
+            <i className="bi bi-chat"></i>
+          </Button>
+          <Button variant="light">
+            <i className="bi bi-repeat"></i>
+          </Button>
+          <Button variant="light" onClick={handleLike}>
+            {isLiked ? (
+              <i className="bi bi-heart-fill text-danger"></i>
+            ) : (
+              <i className="bi bi-heart"></i>
+            )}
+            {likes.length}
+          </Button>
+          <Button variant="light">
+            <i className="bi bi-graph-up"></i>
+          </Button>
+          <Button variant="light">
+            <i className="bi bi-upload"></i>
+          </Button>
+        </div>
+      </Col>
+    </Row>
+  )
+}
+
